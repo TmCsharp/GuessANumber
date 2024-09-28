fff92e01483188fcb37cf34781bc81c25af07b41
TmCsharp <HugonunAmk007>
2024-09-28 12:05:53 +0300

    Project Guess a number.

---
+++ GuessANumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GuessANumberGame
+{
+    internal class GuessANumber
+    {
+        static void Main(string[] args)
+        {
+            Random randomNum = new Random();
+            int pcNumber = randomNum.Next(1, 101);
+            int numCheck = pcNumber;
+
+
+            while (true)
+            {
+
+                Console.Write("Guess a number (1-100): ");
+                string input = Console.ReadLine();
+                bool valid = int.TryParse(input, out numCheck);
+
+
+                if (valid)
+                {
+
+                    int num = int.Parse(input);
+
+                    if (pcNumber == num)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (pcNumber < num)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else if (pcNumber > num)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+            }
+        }
+    }
+}
