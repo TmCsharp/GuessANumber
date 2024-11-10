1cfc0dde4264aa20c1a19ae894d51e90a478383c
TM <115100205+TmCsharp@users.noreply.github.com>
2024-11-10 11:23:25 +0200

    Update GuessANumber.cs

--- GuessANumber.cs
+++ GuessANumber.cs
@@ -7,14 +7,14 @@ namespace GuessANumberGame
         static void Main(string[] args)
         {
             Random randomNum = new Random();
-            int pcNumber = randomNum.Next(1, 101);
+            int pcNumber = randomNum.Next(1, 151);
             int numCheck = pcNumber;
 
 
             while (true)
             {
 
-                Console.Write("Guess a number (1-100): ");
+                Console.Write("Guess a number (1-150): ");
                 string input = Console.ReadLine();
                 bool valid = int.TryParse(input, out numCheck);
 
